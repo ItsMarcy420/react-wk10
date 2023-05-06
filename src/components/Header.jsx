6f2a49d68f71e935073f3f93edab8164cf44113e
ItsMarcy420 <inori.t789@gmail.com>
2023-05-06 20:54:07 +0800

    add firebase

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ export default function Header(){
     <header className="header">
       <div className="container d-flex flex-column align-items-center">
         <img className="header__avatar" src="./images/avatar.png" alt="avatar"/>
-        <h1 className="header__title">INXUAN</h1>
+        <h1 className="header__title">AAAAAA</h1>
         <hr className="divider--light"/>
-        <p className="header__slogan">dadiogaosai</p>
+        <p className="header__slogan">AAAAAAAAA</p>
       </div>
     </header>
     );
